bcf8eff457b56cc309d102af35748cce507c73a4
Arjun <dhanu2060@gmail.com>
2019-05-22 19:18:49 +0530

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray=[]
+
+  addTodo(value){
+    this.todoArray.push(value)
+    console.log(this.todoArray)
+  }
+  
+  /*delete item*/
+  deleteItem(todo){
+   for(let i=0 ;i<= this.todoArray.length ;i++){
+    if(todo== this.todoArray[i]){
+     this.todoArray.splice(i,1)
+    }
+   }
+  }
+
+  // submit Form
+  todoSubmit(value:any){
+     if(value!==""){
+    this.todoArray.push(value.todo)
+     //this.todoForm.reset()
+    }else{
+      alert('Field required **')
+    }
+    
+  }
+  
 }
